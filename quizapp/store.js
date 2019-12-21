76f6a9831096b56dea77a0d498f7dd30c0553c3b
Chanda Hubbard <chanda@thinkful.com>
2019-12-21 15:02:05 -0800

    Initial commit

---
+++ quizapp/store.js
@@ -0,0 +1,115 @@
+const STORE = {
+    questions: [ //1
+        {
+            question: "Which fashion designer is credited with introducing the”Little Black Dress” or “LBD”?",
+            options: [
+                "Vera Wang",
+                "Yves Saint Laurent",
+                "Gabrielle Coco Chanel",
+                "Kate Spade"
+            ],
+            answer "Gabrielle Coco Chanel"
+        },
+        //2
+        {
+            question: "As much as women love a “Little Black Dress”, which fashion designer is responsible for introducing us to the versatile, figure-flattering, jersey wrap-dress silhouette?",
+            options: [
+                "Vivienne Westwood",
+                "Betsey Johnson",
+                "Carolina Herrera",
+                "Diane Von Furstenberg"
+            ],
+            answer "Diane Von Furstenberg"
+        },
+        //3
+        {
+            question: "Which historic English brand is known for developing thier iconic trench coat during World War I?",
+            options: [
+                "Halston",
+                "Burberry",
+                "Club Monaco",
+                "Dolce & Gabanna"
+            ],
+            answer "Burberry"
+        },
+        //4
+        {
+            question: "Which French shoe designer is known for his eye catching red-bottom shoes, and is a leader in the trend toward hgher heel heights?",
+            options: [
+                "Manolo Blahnik",
+                "Tom Ford",
+                "Christian Louboutin",
+                "Dior"
+            ],
+            answer "Christian Louboutin"
+        },
+        //5
+        {
+            question: "Ladies love thier bags, which brand is responsible for creating the most wanted Birkin bag, named after British actress Jane Birkin?",
+            options: [
+                "Louis Vuitton",
+                "Hermes",
+                "Celine",
+                "Gucci"
+            ],
+            answer "Hermes"
+        },
+        //6
+        {
+            question: "Fashion brand Miu Miu is a subsidiary of which famous Italian Fashion House?",
+            options: [
+                "Prada",
+                "Valentino",
+                "Marc Jacobs",
+                "Dries Van Noten"
+            ],
+            answer "Prada"
+        },
+        //7
+        {
+            question: "Which fashion designers' real name is Ralph Lifshitz?",
+            options: [
+                "Alexander Wang",
+                "Giorgio Armani",
+                "Karl Lagerfeld",
+                "Ralph Lauren"
+            ],
+            answer "Ralph Lauren"
+        },
+        //8
+        {
+            question: "This brands' crocodile loga was created in 1933, and was the first ever designer logo.",
+            options: [
+                "Givenchy",
+                "Balenciaga",
+                "Lacoste",
+                "Fendi"
+            ],
+            answer "Lacoste"
+        },
+        //9
+        {
+            question: "This environmentally conscious designer is the daughter of Beatles legend Sir Paul McCartney.",
+            options: [
+                "Stella McCartney",
+                "Stella Artois",
+                "Ronald McCartney",
+                "Anna Sui"
+            ],
+            answer: "Stella McCartney"
+        },
+        //10
+        {
+            question: "This designers' muse was his sister, Donatella.",
+            options: [
+                "Alexander McQueen",
+                "Calvin Klein",
+                "Oscar de la Renta",
+                "Gianni Versace"
+            ],
+            answer: "Gianni Versace"
+        }
+    ],
+    currentQuestion: 0,
+    score: 0
+};
